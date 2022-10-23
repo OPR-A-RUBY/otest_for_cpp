2969a7d516d51badda40667f63045448a70eb6ab
OPR-A-RUBin <opr.a.ruby@gmail.com>
2022-10-23 13:25:38 +0400

    "+ - * /"

--- main.cpp
+++ main.cpp
@@ -18,7 +18,16 @@ int main() {
     cin >> num_2;
     
     result = num_1 + num_2;
-    cout << "Result = " << result << endl;
+    cout << num_1 << " + " << num_2 << " = " << result << endl;
+    
+    result = num_1 - num_2;
+    cout << num_1 << " - " << num_2 << " = " << result << endl;
+    
+    result = num_1 * num_2;
+    cout << num_1 << " * " << num_2 << " = " << result << endl;
+    
+    result = num_1 / num_2;
+    cout << num_1 << " / " << num_2 << " = " << result << endl;
     
     return 0;
 }
