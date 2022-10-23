73e1269cebbdcac18be62985b20dcf47b022c553
OPR-A-RUBin <opr.a.ruby@gmail.com>
2022-10-23 19:01:32 +0400

    Add CONSTRUCTOR

--- main.cpp
+++ main.cpp
@@ -11,17 +11,28 @@ using namespace std;
 #include "functions.cpp"
 
 class Car {
+  
 private:
   int height;
   float speed;
+  
 public:
+  Car () {}  				// Конструктор без параметров
+  
+  Car (int height, float speed) { 	// Конструктор с параметрами
+    this->height = height;
+    this->speed = speed;
+  }
+  
   void setValues (int height, float speed) {
     this->height = height;
     this->speed = speed;  
   }
+  
   void print () {
     cout << this->height << " - " << this->speed << endl;
   }
+  
 };
   
 int main() {
@@ -44,8 +55,7 @@ int main() {
     
     cout << "_____________ CLASS ______________________________________" << endl;
     
-    Car shkoda;
-    shkoda.setValues(1100, 234.4);
+    Car shkoda(1100, 234.4);
     shkoda.print ();
     
     Car audi;
