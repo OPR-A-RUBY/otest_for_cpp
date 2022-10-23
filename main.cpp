a802a34fc11ce16a0c8bef8d69763c6665c556d3
OPR-A-RUBin <opr.a.ruby@gmail.com>
2022-10-23 16:44:52 +0400

    Reconfigure

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 #include "variables_type.cpp"
 #include "algebras.cpp"
 #include "if_then.cpp"
+#include "arrays.cpp"
 
 int main() {
     
@@ -16,29 +17,8 @@ int main() {
     
     if_then();  // Условия и ветвления 
     
+    arrays();   // Массивы
     
-    cout << "_____________ ARRAY ________________________________________" << endl;
-    
-    int array_i[4];
-    array_i[0] = 23;
-    
-    cout << array_i[0] << endl;
-    
-    
-    
-    float array[5];
-    array[0] = 12.45;
-    
-    cout << array[0] << endl;
-    
-    
-    
-    int arr[3] = {23, 56, 12};  // Инициализация со значениями  (одномерный)
-    
-    int arr0[3][2] = {{12, 34}, {56, 78}, {90, 11}};  // (двумерный)
-    
-    cout << arr[2] << endl;      // Это 12 в первом массиве
-    cout << arr0[1][0] << endl;  // Это 56 во втором массиве
     
     return 0;
 }
