af8c9070984ad56833e21e1a8790c6ac15b22caf
OPR-A-RUBin <opr.a.ruby@gmail.com>
2022-10-23 12:57:47 +0400

    reconfigure code

---
+++ variables_type.cpp
@@ -0,0 +1,28 @@
+int var_type() {
+    
+    char sym = 'A';
+    cout << "char = " << sym << endl;
+    
+    bool isWork = true;
+    cout << "boll = " << isWork << endl;
+    
+    int num = 1;
+    long int num_1 = 23456787654321;
+    short int num_2 = 72;
+    cout << "int = " << num << endl;
+    cout << "long int = " << num_1 << endl;
+    cout << "short int = " << num_2 << endl;
+    
+    float favNum = 3.72f;
+    float favNum1 = .72;
+    cout << "float = " << favNum << endl;
+    cout << "float = " << favNum1 << endl;
+    
+    double test = 12354885.12458759548368001;
+    cout << "double = " << test << endl;
+    
+    const float p = 3.141593;
+    cout << "const = " << p << endl;
+    
+    return 0;
+}
