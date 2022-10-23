82bdd837291dcbbf4665cb5dbae64a563c207d56
OPR-A-RUBin <opr.a.ruby@gmail.com>
2022-10-23 18:08:55 +0400

    Best function

--- functions.cpp
+++ functions.cpp
@@ -7,10 +7,16 @@ void print (string word, float a) {
   }
   
 float del (float a, float b) {
-  if (b ==0)
-    return 0;
-  else
-    return a / b;
+  float res;
+  try {
+    if (b !=0)
+      res = a / b;
+    else
+      throw (b);
+    } catch (int number) {
+	res = a;
+    } 				// Time = 0.52.00
+  return res;
   }
 
 int function() {
