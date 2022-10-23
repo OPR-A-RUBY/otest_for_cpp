a13aa74e4d0587a82125d832ee34bb1466a27bb3
OPR-A-RUBin <opr.a.ruby@gmail.com>
2022-10-23 12:26:32 +0400

    INTEGER - variables

--- main.cpp
+++ main.cpp
@@ -13,5 +13,12 @@ int main() {
     bool isWork = true;
     cout << isWork << endl;
     
+    int num = 1;
+    long int num_1 = 23456787654321;
+    short int num_2 = 72;
+    cout << "int = " << num << endl;
+    cout << "long int = " << num_1 << endl;
+    cout << "short int = " << num_2 << endl;
+    
     return 0;
 }
