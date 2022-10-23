377911987105feee8274a2d6b2ac8d57d99008b3
OPR-A-RUBin <opr.a.ruby@gmail.com>
2022-10-23 17:47:11 +0400

    My PRINT  and  function (return float type)

--- main.cpp
+++ main.cpp
@@ -13,6 +13,17 @@ void print (string word) {
   cout << word << endl;
   }
 
+void print (string word, float a) {
+  cout << word << " " << a << endl;
+  }
+  
+float del (float a, float b) {
+  if (b ==0)
+    return 0;
+  else
+    return a / b;
+  }
+  
 int main() {
     
     cout << "Hello World!" << endl;
@@ -33,6 +44,9 @@ int main() {
     
     string hi = "Hello, big World!";
     print (hi);
+    
+    float res = del (23, 2);
+    print ("Result is", res);
     
     return 0;
 }
